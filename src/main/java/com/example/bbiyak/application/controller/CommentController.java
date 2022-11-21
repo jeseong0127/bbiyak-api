bc3e6f42b585f1cdb4c15cbbe79ccfbf5f4981ae
heojaewon <wizard11292@gmail.com>
2022-11-21 12:08:52 +0900

    Feat: 댓글 수정 api 개발

--- src/main/java/com/example/bbiyak/application/controller/CommentController.java
+++ src/main/java/com/example/bbiyak/application/controller/CommentController.java
@@ -1,6 +1,8 @@
 package com.example.bbiyak.application.controller;
 
 import com.example.bbiyak.application.request.CommentRequest;
+import com.example.bbiyak.application.request.UpdateCommentRequest;
+import com.example.bbiyak.application.request.UpdateNoticeRequest;
 import com.example.bbiyak.domain.service.CommentService;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
@@ -22,6 +24,15 @@ public class CommentController {
            @RequestBody CommentRequest commentRequest
     ){
         commentService.insertComment(commentRequest);
+    }
+
+    @ApiOperation("댓글 수정")
+    @PutMapping()
+    @ResponseStatus(HttpStatus.OK)
+    public void updateComment(
+            @RequestBody UpdateCommentRequest updateCommentRequest
+            ){
+        commentService.updateComment(updateCommentRequest);
     }
 
 
--- src/main/java/com/example/bbiyak/domain/service/CommentService.java
+++ src/main/java/com/example/bbiyak/domain/service/CommentService.java
@@ -1,10 +1,16 @@
 package com.example.bbiyak.domain.service;
 
 import com.example.bbiyak.application.request.CommentRequest;
+import com.example.bbiyak.application.request.UpdateCommentRequest;
+import com.example.bbiyak.application.request.UpdateNoticeRequest;
+import com.example.bbiyak.domain.exception.CommentNotFoundException;
 import com.example.bbiyak.domain.model.entity.Comment;
 import com.example.bbiyak.domain.model.repository.CommentRepository;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.sql.Update;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -14,5 +20,12 @@ public class CommentService {
 
     public void insertComment(CommentRequest commentRequest){
         commentRepository.save(new Comment(commentRequest));
+    }
+
+    public void updateComment(UpdateCommentRequest updateCommentRequest){
+            Comment comment = commentRepository.findById(updateCommentRequest.getCommentNo()).orElseThrow(CommentNotFoundException::new);
+            comment.setCommentContent(updateCommentRequest.getCommentContent());
+            comment.setRating(updateCommentRequest.getRating());
+            commentRepository.save(comment);
     }
 }
